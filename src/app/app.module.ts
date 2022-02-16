810eca0a3dd6c3ac68207ef7ef34b27e44a4634a
Julian <julian.hernandez.carrion@nttdata.com>
2022-02-16 13:03:49 +0100

    Fin día 7 bootcamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { Routes, RouterModule } from '@angular/router';
+import  { HttpClientModule  } from '@angular/common/http'; 
 
 import { ProductosService } from './servicio/productos.service';
+import { UsuariosService } from './servicio/usuarios.service';
+
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HijoPrComponent } from './hijo-pr/hijo-pr.component';
 import { CSeccion2Component } from './c-seccion2/c-seccion2.component';
 import { CSeccion3Component } from './c-seccion3/c-seccion3.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 
 const routes: Routes = [
@@ -19,6 +23,7 @@ const routes: Routes = [
   {path:'seccion3', component: CSeccion3Component},
   {path:'seccion2/:nombre', component: CSeccion2Component},
   {path:'seccion3/:ciudad/:poblacion', component: CSeccion3Component},
+  {path: 'usuario', component: UsuarioComponent},
   {path:'**', component: CSeccion2Component}
 ];
 
@@ -31,15 +36,19 @@ const routes: Routes = [
     FooterComponent,
     HijoPrComponent,
     CSeccion2Component,
-    CSeccion3Component
+    CSeccion3Component,
+    UsuarioComponent
   ], 
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
-  providers: [ProductosService],
+  providers: [
+    ProductosService, 
+    UsuariosService],
   bootstrap: [PrincipalComponent]
 })
 export class AppModule { }
